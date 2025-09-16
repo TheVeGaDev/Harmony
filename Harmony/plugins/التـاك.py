2ceefaa97414bd5747e013942f239fa46ba5b6fb
TheVeGaDev <m.medhat2610@gmail.com>
2025-09-16 11:05:20 +0300

    Update Harmony/plugins/التـاك.py from Harmony.zip

--- Harmony/plugins/التـاك.py
+++ Harmony/plugins/التـاك.py
@@ -20,7 +20,7 @@ from telethon.tl import functions
 
 from telethon.tl.types import ChannelParticipantsAdmins
 
-from Tepthon import zedub
+from Harmony import zedub
 
 from ..core.logger import logging
 from ..core.managers import edit_delete, edit_or_reply
