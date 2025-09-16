b2bbeda6c5e027f2a19ccece4aec00ad7fdead12
TheVeGaDev <m.medhat2610@gmail.com>
2025-09-16 11:04:39 +0300

    Update Harmony/plugins/الحمايه.py from Harmony.zip

--- Harmony/plugins/الحمايه.py
+++ Harmony/plugins/الحمايه.py
@@ -6,8 +6,8 @@ from telethon import Button, functions
 from telethon.events import CallbackQuery
 from telethon.utils import get_display_name
 
-from Tepthon import zedub
-from Tepthon.core.logger import logging
+from Harmony import zedub
+from Harmony.core.logger import logging
 
 from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
